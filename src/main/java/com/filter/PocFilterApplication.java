3aab70e0951ff8c6b6ac8f56a9d122a9c8713e30
Bryan Lucas <bryan@pop-os.localdomain>
2022-03-22 19:59:55 -0300

    Adding base project structure

---
+++ src/main/java/com/filter/PocFilterApplication.java
@@ -0,0 +1,13 @@
+package com.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocFilterApplication.class, args);
+	}
+
+}
